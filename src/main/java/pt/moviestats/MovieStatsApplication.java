41531121f837f01b782d64c340a746f709723b1d
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2024-01-10 21:28:21 +0000

    Setup project

---
+++ src/main/java/pt/moviestats/MovieStatsApplication.java
@@ -0,0 +1,13 @@
+package pt.moviestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStatsApplication.class, args);
+	}
+
+}
