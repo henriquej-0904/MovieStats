d58054c0c5de6a50aebe7db757f9c73bc1c15d7b
henriquej-0904 <hjp.ferreira@campus.fct.unl.pt>
2024-01-11 01:04:04 +0000

    add movie repo and domain service layer

--- src/main/java/pt/moviestats/data/MovieRepo.java
+++ src/main/java/pt/moviestats/data/MovieRepo.java
@@ -1,9 +1,13 @@
 package pt.moviestats.data;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface MovieRepo extends CrudRepository<MovieDAO, Long> {
 
-    
+    List<MovieDAO> findByTitleLike(String title);
+
+    List<MovieDAO> findByDateBetween(long from, long to);
 
 }
--- src/main/java/pt/moviestats/domain/MoviesDomainService.java
+++ src/main/java/pt/moviestats/domain/MoviesDomainService.java
@@ -1,11 +1,14 @@
 package pt.moviestats.domain;
 
 import java.security.InvalidParameterException;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import pt.moviestats.data.MovieDAO;
 import pt.moviestats.data.MovieRepo;
+import pt.moviestats.data.MovieDAO.UpdateMovieDAO;
+import pt.moviestats.exceptions.MovieNotFoundException;
 
 @Service
 public class MoviesDomainService {
@@ -27,15 +30,49 @@ public class MoviesDomainService {
         return movieRepo.save(movie);
     }
 
+    public MovieDAO updateMovie(long id, UpdateMovieDAO update) {
+        checkMovieFields(update);
+        
+        final var m = movieRepo.findById(id).
+            orElseThrow(() -> MovieNotFoundException.fromMovieId(id));
+
+        m.setTitle(update.title());
+        m.setDate(update.date());
+        m.setRank(update.rank());
+        m.setRevenue(update.revenue());
+
+        return movieRepo.save(m);
+    }
+
+    public void deleteMovieById(long id) {
+        if (!movieRepo.existsById(id))
+            throw MovieNotFoundException.fromMovieId(id);
+
+        movieRepo.deleteById(id);
+    }
+
+    public List<MovieDAO> getAllMoviesFilteredByLaunchDate(long from, long to) {
+        if (from <= 0 || to <= 0 || from > to)
+            throw new InvalidParameterException("Launch date filter is composed by a start and end dates.");
+        
+        return movieRepo.findByDateBetween(from, to);
+    }
+
     void checkMovieFields(MovieDAO movie) {
-        String title = movie.getTitle();
+        checkMovieFields(movie.getTitle(), movie.getDate(), movie.getRank());
+    }
+
+    void checkMovieFields(UpdateMovieDAO movie) {
+        checkMovieFields(movie.title(), movie.date(), movie.rank());
+    }
+
+    void checkMovieFields(String title, long date, int rank) {
         if (title == null || title.isBlank())
             throw new InvalidParameterException("The title must be a valid string of characters.");
 
-        if (movie.getLaunchDate() <= 0)
+        if (date <= 0)
             throw new InvalidParameterException("The launch date must be a valid date.");
 
-        int rank = movie.getRank();
         if (rank < MIN_RANK || rank > MAX_RANK)
             throw new InvalidParameterException(
                 String.format("The rank of the movie must be a value between %d and %d.",
